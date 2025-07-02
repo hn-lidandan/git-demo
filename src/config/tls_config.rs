776fdae04b71c8b46e10a636ab5d7ac9bf23802a
lidandan <lidandan@dy-sec.com>
2025-07-02 12:59:22 +0800

    修改协议支持

--- src/config/tls_config.rs
+++ src/config/tls_config.rs
@@ -1,54 +1,57 @@
-use anyhow::{Context, Result};
-use rustls::{Certificate, PrivateKey, ServerConfig};
-use rustls_pemfile::{certs, Item, read_one};  // 使用 read_one 函数
-use std::fs::File;
-use std::io::BufReader;
-use std::path::Path;
-use log::{info};
-use rustls::version::{TLS12}; 
+use actix_web::dev::{Service, Transform, ServiceRequest, ServiceResponse};
+use actix_web::Error;
+use futures::future::{ok, Ready};
+use log::info;
 
-pub fn load_rustls_config(
-    cert_path: impl AsRef<Path>,
-    key_path: impl AsRef<Path>,
-) -> Result<ServerConfig> {
-    info!("进入证书加载");
-    
-    // 加载证书链
-    let cert_file = File::open(cert_path).context("打开证书文件失败")?;
-    let mut cert_reader = BufReader::new(cert_file);
-    let cert_chain = certs(&mut cert_reader)?
-        .into_iter()
-        .map(Certificate)
-        .collect();
-    
-    // 加载私钥 - 使用 read_one 函数
-    let key_file = File::open(key_path).context("打开私钥文件失败")?;
-    info!("加载私钥");
-    let mut key_reader = BufReader::new(key_file);
-    
-    // 使用 read_one 自动检测格式
-    let key = match read_one(&mut key_reader)? {
-        Some(Item::PKCS8Key(key)) => PrivateKey(key),
-        Some(Item::RSAKey(key)) => PrivateKey(key),
-        Some(Item::ECKey(key)) => PrivateKey(key),
-        _ => anyhow::bail!("无法识别的私钥格式"),
-    };
-    
-    info!("创建TLS配置");
-    // 创建TLS配置 - 使用兼容性更好的设置
-    let mut config = ServerConfig::builder()
-        .with_safe_default_cipher_suites() // 使用安全默认值
-        .with_safe_default_kx_groups()     // 使用安全默认值
-        .with_protocol_versions(&[&TLS12])? // 只使用 TLS 1.2 提高兼容性
-        .with_no_client_auth()
-        .with_single_cert(cert_chain, key)
-        .context("创建TLS配置失败")?;
+pub struct SimpleLogger;
 
-    // 同时支持 HTTP/2 和 HTTP/1.1
-    config.alpn_protocols = vec![
-        b"http/1.1".to_vec()  // 只支持 HTTP/1.1 提高兼容性
-    ];
-    info!("已设置ALPN协议为HTTP/1.1");
-    
-    Ok(config)
+impl<S, B> Transform<S, ServiceRequest> for SimpleLogger
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type Transform = SimpleLoggerMiddleware<S>;
+    type InitError = ();
+    type Future = Ready<Result<Self::Transform, Self::InitError>>;
+
+    fn new_transform(&self, service: S) -> Self::Future {
+        ok(SimpleLoggerMiddleware { service })
+    }
+}
+
+pub struct SimpleLoggerMiddleware<S> {
+    service: S,
+}
+
+impl<S, B> Service<ServiceRequest> for SimpleLoggerMiddleware<S>
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type Future = futures::future::LocalBoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(&self, cx: &mut std::task::Context<'_>) -> std::task::Poll<Result<(), Self::Error>> {
+        self.service.poll_ready(cx)
+    }
+
+    fn call(&self, req: ServiceRequest) -> Self::Future {
+        let path = req.path().to_string();
+        let method = req.method().to_string();
+
+        info!("请求: {} {}", method, path);
+
+        let fut = self.service.call(req);
+
+        Box::pin(async move {
+            let res = fut.await?;
+            info!("响应: {}", res.status());
+            Ok(res)
+        })
+    }
 }
